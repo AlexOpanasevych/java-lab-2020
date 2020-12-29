ceac1d9e1d587389cabe9043c50f69a2fcc76519
Oleksandr Opanasevych <karabas2903@gmail.com>
2020-12-29 14:00:50 +0200

    added maven surefire and spy test, renamed file

--- musicinstrument/test/MusicInstrumentIT.java
+++ musicinstrument/test/MusicInstrumentIT.java
@@ -14,4 +14,16 @@ public class MusicInstrumentTestMockito {
         when(mi.type()).thenReturn(MIType.STRING);
         assertEquals(mi.type(), MIType.STRING);
     }
+
+    public void addPart_PartValid_True() {
+        ArrayList<MusicInstrument> spyList = spy(new ArrayList<MusicInstrument>());
+
+        spyList.add(new Body(1.2));
+        spyList.add(new Tune(2.3));
+
+        Mockito.verify(spyList).add(Body::class);
+        Mockito.verify(spyList).add(Tune::class);
+
+        assertEquals(2, spyList.size());
+    }
 }
